89caeab2718cd3860896fb98fef2370629be45f3
NatanSunelaitis <natansunelaitis@gmail.com>
2022-05-06 14:49:13 -0300

    :sparkles: finishing basic types class 1: 485

---
+++ fundamentos/tipos_basicos_1.dart
@@ -0,0 +1,27 @@
+/*
+  - Números (int e double)
+  - String (String)
+  - Booleano (bool)
+ */
+
+main() {
+  int n1 = 3;
+  double n2 = (-5.67).abs();
+  double n3 = double.parse("12.765");
+  num n4 = 6;
+
+  print(n1.abs() + n2 + n3 + n4);
+
+  n4 = 6.7;
+  print(n1.abs() + n2 + n3 + n4);
+
+  String s1 = "Bom dia";
+  String s2 = " dia";
+
+  print(s1 + s2.toUpperCase() + "!!!");
+
+  bool estaChovendo = true;
+  bool muitoFrio = false;
+
+  print(estaChovendo && muitoFrio);
+}
