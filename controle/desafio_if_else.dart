42e30e630ea05c2252ad88115ca5b9c1ee830a13
NatanSunelaitis <natansunelaitis@gmail.com>
2022-05-11 17:02:47 -0300

    :sparkles: Finishing challenge about conditional struture: 498

---
+++ controle/desafio_if_else.dart
@@ -0,0 +1,10 @@
+main() {
+  var nota = 0.3;
+
+  if (nota >= 9.0) // ; -> Não usa ; em estruturas de controle *
+  {
+    print('Parabéns! Você foi brilhante!');
+  }
+
+  // * Exceção do/While!!!
+}
